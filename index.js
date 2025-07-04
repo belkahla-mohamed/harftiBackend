f9463cf0d49fe0ac22a305755813a5ffcce61ee7
Mouadlamsila <mouad.lamssila@gmail.com>
2025-07-04 20:09:27 +0100

    try

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const http = require("http"); // Required for Socket.io
-const { Server } = require("socket.io");
 
 const authRoutes = require("./routes/authRoutes");
 const serviceRoutes = require("./routes/serviceRoutes");
@@ -13,13 +11,6 @@ const postRoutes = require("./routes/postEmployee");
 const reservationRoutes = require("./routes/reservationRoutes");
 
 const app = express();
-const server = http.createServer(app); // Create HTTP server
-const io = new Server(server, {
-  cors: {
-    origin: "https://harftii.web.app", // Change to your frontend URL
-    methods: ["GET", "POST"],
-  },
-});
 
 app.use(express.json());
 app.use(cors());
@@ -28,22 +19,6 @@ app.use(cors());
 mongoose.connect("mongodb+srv://devmohamedbelkahla:uCMzWAlgTVWZGisi@cluster1.lrw6fh3.mongodb.net/harfti?retryWrites=true&w=majority&appName=Cluster1")
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("MongoDB connection error:", err));
-
-// Socket.io Connection
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
-
-  socket.on("newPost", (data) => {
-    io.emit("updatePosts", data); // Broadcast the new post to all clients
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
-
-// Attach io to req.app for routes to access it
-app.set("socketio", io);
 
 // Route Middleware
 app.use("/auth", authRoutes);
@@ -62,6 +37,7 @@ app.use("/servicesPhotos", express.static("servicesPhotos"));
 app.use("/PostPhoto", express.static("PostPhoto"));
 
 // Start Server
-server.listen(3001, () => {
-    console.log("Server is running on port 3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
